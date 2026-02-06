88b8c82f53f741358e097cbaa349b5a8c5cb858a
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2026-02-06 17:35:45 +0200

    add basic displayHome function

--- src/home.js
+++ src/home.js
@@ -1,3 +1,19 @@
+import { createElement } from "react";
+
 const home = "Home page";
+
+function displayHome() {
+    const content = document.querySelector("#content")
+    content.innerHTML = "";
+
+    let homeContainer = createElement("div");
+    homeContainer.id.add("home-container");
+    content.appendChild(homeContainer);
+
+    let homeTitle = createElement("h1");
+    homeTitle.textContent = "Add Home title here";
+    homeTitle.id.add("home-title");
+    homeTitle.appendChild(homeContainer)
+}
 
 export {home}
