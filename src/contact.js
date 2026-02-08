a0aa2ad0cb087ab46b429bae51dc6b0d11fba07a
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2026-02-08 08:46:39 +0200

    add basic styling for menu and contacts content, title and subtitles

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,18 @@
 export function displayContact() {
     const content = document.querySelector("#content");
+    content.innerHTML = "";
 
-    content.innerHTML = ""
+    let contactContainer = createElement("div");
+    contactContainer.setAttribute("id", "contact-container")
+    content.appendChild(contactContainer);
+
+    let contactTitle = createElement("h2");
+    contactTitle.textContent = "Contact Header Text";
+    contactTitle.setAttribute("id", "contact-title");
+    contactContainer.appendChild(contactTitle);
+
+    let contactSubtitle = createElement("h3");
+    contactSubtitle.textContent = "Contact Subtitle Text";
+    contactSubtitle.setAttribute("id", "contact-subtitle");
+    contactContainer.appendChild(contactSubtitle);
 }
